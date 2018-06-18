fe36ee27f761793ea87803e688778d4f69071af3
Nathan Schwartz <cgtnathan@gmail.com>
2018-06-18 15:12:08 -0400

    CORS

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ app.use('/messaging', messaging);
 app.use('/balances', balances);
 
 app.use((req, res, next) => {
-  res.header({"Access-Control-Allow-Origin": "*"});
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -60,6 +59,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
     return res.status(200).json({});
   }
+  next();
 });
 
 // catch 404 and forward to error handler
