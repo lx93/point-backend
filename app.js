092f9d1a36f481fa7feff8ecf1957acf22eec4cc
Nathan Schwartz <cgtnathan@gmail.com>
2018-06-18 15:08:16 -0400

    CORS

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ app.use('/messaging', messaging);
 app.use('/balances', balances);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin": "*");
+  res.header({"Access-Control-Allow-Origin": "*"});
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
