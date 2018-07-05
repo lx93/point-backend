5434f56f7a10e9e0479546d96f1465967492c724
Nathan Schwartz <cgtnathan@gmail.com>
2018-07-05 11:54:29 -0400

    messaging

--- utils/messenger.js
+++ utils/messenger.js
@@ -7,7 +7,7 @@ function createCard(merchantName, balance, balanceId, phone) {
   var output = "";
   output += "You just obtained a giftcard with " + merchantName + "!\n";
   output += "Your balance with " + merchantName + " is now " + formatMoney(balance) + ".\n";
-  output += "View and redeem your Pointup Giftcard at app.point.io/qr/" + balanceId + ".\n";
+  output += "View and redeem your Pointup Giftcard at " + process.env.WEBAPP_URL + "qr/" + balanceId + ".\n";
   output += "Recipient: " + phone;
   return output;
 }
@@ -16,7 +16,7 @@ function updateCard(merchantName, balance, balanceId, phone) {
   var output = "";
   output += "Your balance with " + merchantName + " has been updated!\n";
   output += "Your balance with " + merchantName + " is now " + formatMoney(balance) + ".\n";
-  output += "View and redeem your Pointup Giftcard at app.point.io/qr/" + balanceId + ".\n";
+  output += "View and redeem your Pointup Giftcard at " + process.env.WEBAPP_URL + "qr/" + balanceId + ".\n";
   output += "Recipient: " + phone;
   return output;
 }
